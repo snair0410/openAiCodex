25dc3a9dc97bd0161ef5b52bf9c5acf500aaeef1
Sandeep Nair <sandeepnair@192.168.1.4>
2023-01-04 10:41:59 +0530

    update server url

--- client/script.js
+++ client/script.js
@@ -75,7 +75,7 @@ const  handleSubmit = async (e) => {
 
   //fetch data from server - bots response
 
-  const response = await fetch ('http://localhost:5000', {
+  const response = await fetch ('https://openaichatbot-0fjg.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
